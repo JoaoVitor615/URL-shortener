c8798326bd85d5fc3450295ad77c277ed4f80d1c
JoaoVitor615 <jvpeniza@gmail.com>
2026-02-03 21:56:01 -0300

    feat: Update ID generation logic to use math.Pow for flexibility and add unit tests

--- internal/idgenerator/constants.go
+++ internal/idgenerator/constants.go
@@ -1,11 +1,19 @@
 package idgenerator
 
-import "github.com/JoaoVitor615/URL-shortener/internal/encoder"
+import (
+	"math"
 
-const (
+	"github.com/JoaoVitor615/URL-shortener/internal/encoder"
+)
+
+// not defined as const because of the math.Pow function
+//
+// it was not defined hardcoded for the flexibility of changing the base and
+// the number of minimum and maximum characters.
+var (
 	// The minimum number for the encoded ID having minimum 5 characters (N^4)
-	minID = encoder.Base ^ 4
+	minID = int(math.Pow(float64(encoder.Base), 4))
 
 	// The maximum number for the encoded ID having maximum 7 characters (N^7 - 1)
-	maxID = encoder.Base ^ 7 - 1
+	maxID = int(math.Pow(float64(encoder.Base), 7) - 1)
 )
