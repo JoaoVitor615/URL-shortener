97a4947434e55cc7a8f59ba88727570a4c18b3f0
JoaoVitor615 <jvpeniza@gmail.com>
2026-02-10 20:28:20 -0300

    feat: Add base62 decoding functions

--- internal/core/encoder/encoder.go
+++ internal/core/encoder/encoder.go
@@ -1,5 +1,10 @@
 package encoder
 
+import (
+	"strings"
+)
+
+// Encode converts a number to a base62 string
 func Encode(num int) string {
 	if num == 0 {
 		return string(alphabet[0])
@@ -11,4 +16,14 @@ func Encode(num int) string {
 		num = num / Base
 	}
 	return encoded
+}
+
+// Decode converts a base62 string to a number
+func Decode(encoded string) int {
+	encodedChars := strings.Split(encoded, "")
+	decoded := 0
+	for i := 0; i < len(encoded); i++ {
+		decoded = decoded*Base + strings.Index(alphabet, encodedChars[i])
+	}
+	return decoded
 }
--- internal/core/encoder/encoder_test.go
+++ internal/core/encoder/encoder_test.go
@@ -11,7 +11,14 @@ func TestEncode(t *testing.T) {
 }
 
 func TestEncode_LargeNumber(t *testing.T) {
-	assert.Equal(t, "1", Encode(1))
+	assert.Equal(t, "1Z", Encode(123))
 	assert.Equal(t, "a", Encode(10))
 	assert.Equal(t, "PNFQ", Encode(12345678))
+}
+
+func TestDecode(t *testing.T) {
+	assert.Equal(t, 123, Decode("1Z"))
+	assert.Equal(t, 10, Decode("a"))
+	assert.Equal(t, 1, Decode("1"))
+	assert.Equal(t, 12345678, Decode("PNFQ"))
 }
